42fb6b770a7b99f07a7115ffb67001cf3437b32a
Dipti <diptiwaghmare28112002@gmail.com>
2025-05-21 16:57:30 +0530

    Done

--- audio_utils.py
+++ audio_utils.py
@@ -1,23 +1,19 @@
-import os
+import librosa
 import torch
 import numpy as np
 from pyannote.audio import Inference
-from huggingface_hub import login
 from sklearn.metrics.pairwise import cosine_similarity
 
-# Login using Streamlit secret
-login(token=os.getenv("HF_TOKEN"))
-
+# Set device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Load pyannote speaker embedding model
 inference = Inference("pyannote/embedding", device=device)
 
+# Extract average embedding for audio file
 def extract_embedding(file_path):
     return inference(file_path).data.mean(axis=0).reshape(1, -1)
 
+# Compute cosine similarity between two embeddings
 def get_similarity_score(embedding1, embedding2):
     return float(cosine_similarity(embedding1, embedding2)[0][0])
-
-
-
-
-
